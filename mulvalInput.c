f9a0cb30ae7b6c33c06982172247d3ee788d20e8
Nathan <nburow@purdue.edu>
2013-11-18 09:55:20 -0500

    parsing

--- mulvalInput.c
+++ mulvalInput.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 FILE *out;
 FILE *in;
@@ -34,17 +35,57 @@ char *getLine()
 	line[i] = '\0';
 	return line;
 }
+char *parseWord(char **tmp)
+{
+	char *word = malloc(size*sizeof(char));
+	memset(word, 0, size*sizeof(char));
+
+	int i = 0;
+	int j = 0;
+
+	while(isblank(*tmp[j]) || ispunct(*tmp[j]))
+		j++;
+
+	printf("%c\n", *tmp[j]);
+
+	while(isdigit(*tmp[j]))
+	{
+		word[i] = *tmp[j];
+		i++;
+		j++;
+	}
+	word[i] = '\0';
+	*tmp = *tmp + i;
+	//printf("%s\n", tmp);
+	return word;
+}
 void writeConnections()
 {
+	//set-up
+	fprintf(out, "/*Attacker located*/\n");
+	fprintf(out, "/*Attacker goal*/\n\n\n");
+	fprintf(out, "/*Connections*/\n");
+
+	//write connections
 	char *line = getLine();
+	char *holder = line;
+	//printf("%s\n", tmp);
 	while(strlen(line) > 0)
 	{
-		printf("%s\n", line);
-		free(line);
+		//printf("%s\n", line);
+		char *word = parseWord(&line);
+		while(strlen(word) > 0)
+		{
+			fprintf(out, "%s ", word);
+			word = parseWord(&line);
+		}
+		fprintf(out, "\n");
+		free(holder);
 		line = getLine();
+		holder = line;
 	}
 }
-//arguments: name of input file.
+//arguments: name of output file.
 int main(int argc, char** argv)
 {
 	out = fopen(argv[1], "w");
