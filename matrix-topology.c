5c736db76b71e86375fc73c63916ceffaef9cbcf
Nathan <nburow@purdue.edu>
2013-11-27 17:29:30 -0500

    network v1.1

--- matrix-topology.c
+++ matrix-topology.c
@@ -17,50 +17,76 @@ double alpha = -2.5;
 int min = 1;
 int max = 10;
 
-//grid globals
-int **grid;  //a grid of locations where a node in the mesh network can reside
-int size_x;
-int size_y;
-int nodes;
+int *eligible;
 
 //adjacency list
 int **graph;
+int nodes;
 
-//settings for adding nodes
-int range = 10;
+void addNodes(int node, int connections)
+{
+	//find the first slot that needs a connection for the node
+	int i = 0;
+	while(graph[node][i] != -1)
+		i++;
 
+	while(connections > 0)
+	{
+		int pairTo = uniform(&seed)*nodes; //select a node at random
+		while(eligible[pairTo] == 0)
+			pairTo = uniform(&seed)*nodes;
+		int j = 0;
+		while(graph[pairTo][j] != -1)
+			j++;
+		
+		//add the mutual connections
+		graph[node][i] = pairTo;
+		i++;
+		graph[pairTo][j] = node;
+		
+		connections--;
+	}
+}
 
+//creates the adjacency list
 void init()
 {
-	grid = (int **)malloc(size_x*sizeof(int *));
-	int i;
-	for(i = 0; i < size_x; i++)
-		grid[i] = (int *)malloc(size_y*sizeof(int));
-
 	graph = (int **)malloc(nodes*(sizeof(int *)));
-	for(i = 0; i < nodes; i++)
-		graph[i] = (int *)malloc(max*(sizeof(int)));
+	for(int i = 0; i < nodes; i++)
+	{
+		graph[i] = (int *)malloc((max+1)*(sizeof(int)));
+	}
+	
+	//add connection to prior node to avoid partition
+	for(int i = 1; i < nodes; i++)
+		graph[i][0] = i -1;
+	
+	//initialize to -1 so you can find end of connection list
+	for(int i = 0; i < nodes; i++)
+	{
+		for(int j = 1; j < max + 1; j++)
+			graph[i][j] = -1;
+	}	
 }
 
 //arg1: number of nodes to add
-//arg2: x dim of grid
-//arg3: y dim of grid
 int main(int argc, char **argv)
 {
 	nodes = atoi(argv[1]);
-	size_x = atoi(argv[2]);
-	size_y = atoi(argv[3]);
+	eligible = (int *)malloc(nodes*sizeof(int));
+	memset(eligible, 0, nodes*sizeof(int));
 
 	init();
-
-	int total = 0;
-	while(total < nodes)
+	
+	//determine how many additional connections each node needs
+	for(int i = 0; i < nodes; i++)
+		eligible[i] = (int) round( power_rng(&seed, alpha, min, max) );
+	
+	//add those connnections as necessary
+	for(int i = 0; i < nodes; i++)
 	{
-		double x = power_rng(&seed, alpha, min, max);
-		x = round(x);
-		int newNodes = (int)(x);
-		//addNodes(newNodes);
-		total += newNodes;
+		if(eligible[i] > 0)
+			addNodes(i, eligible[i]);
 	}
 
 	return EXIT_SUCCESS;
